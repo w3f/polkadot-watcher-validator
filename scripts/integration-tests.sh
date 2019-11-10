b885dc7f5ec9ae15beed55e87022542a74febe80
Federico Gimenez <fgimenez@coit.es>
2019-11-10 08:22:59 +0100

    integration test

---
+++ scripts/integration-tests.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+source /scripts/common.sh
+source /scripts/bootstrap-helm.sh
+
+
+run_tests() {
+    echo Running tests...
+
+    wait_pod_ready polkadot-watcher
+}
+
+teardown() {
+    helm delete --purge kusama-faucet
+}
+
+main(){
+    if [ -z "$KEEP_W3F_POLKADOT_WATCHER" ]; then
+        trap teardown EXIT
+    fi
+
+    /scripts/build-helm.sh \
+        --set environment=ci \
+        --set image.tag="${CIRCLE_SHA1}" \
+        polkadot-watcher \
+        ./charts/polkadot-watcher
+
+    run_tests
+}
+
+main
