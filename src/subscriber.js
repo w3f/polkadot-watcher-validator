62a33a924342ada7fc57e327d2f74120d1fd619c
Federico Gimenez <fgimenez@coit.es>
2019-11-10 12:05:54 +0100

    subscribe to changes in accounts nonces for detecting transactions

--- src/subscriber.js
+++ src/subscriber.js
@@ -1,25 +1,41 @@
 const { ApiPromise, WsProvider } = require('@polkadot/api')
 
+const { asyncForEach } = require('./async')
+
 class Subscriber {
-  constructor(endpoint) {
+  constructor(endpoint, subscribe) {
     this.provider = new WsProvider(endpoint)
+    this.subscribe = subscribe
+    this.unsubscribe = {}
   }
 
   async start() {
     await this._initAPI()
+
+    await this._subscribeTransactions(this.subscribe.transactions)
   }
 
   async _initAPI() {
-    const api = await ApiPromise.create({ provider: this.provider })
+    this.api = await ApiPromise.create({ provider: this.provider })
 
     const [chain, nodeName, nodeVersion] = await Promise.all([
-      api.rpc.system.chain(),
-      api.rpc.system.name(),
-      api.rpc.system.version()
+      this.api.rpc.system.chain(),
+      this.api.rpc.system.name(),
+      this.api.rpc.system.version()
     ])
     console.log(
       `You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`
     )
+  }
+
+  async _subscribeTransactions(accounts) {
+    this.unsubscribe.transactions = []
+    await asyncForEach(accounts, async (account) => {
+      const unsub = await this.api.query.system.accountNonce(account.address, (nonce) => {
+        console.log(`The nonce for ${account.name} is ${nonce}`)
+      })
+      this.unsubscribe.transactions.push(unsub)
+    })
   }
 }
 
