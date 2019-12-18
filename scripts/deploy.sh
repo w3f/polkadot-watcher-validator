f9e1991f8da74b6c8b591c40c69881d309bd5098
Federico Gimenez <federico.gimenez@gmail.com>
2019-12-18 13:43:59 +0100

    fix deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
 /scripts/deploy.sh -t helm -c engineering \
-                   --set image.tag="${CIRCLE_TAG}" \
-                   polkadot-watcher \
-                   w3f/polkadot-watcher
+                   -a "--set image.tag=${CIRCLE_TAG} polkadot-watcher w3f/polkadot-watcher"
