ea88fe568061d06001e7186e88fa0f1cfcd02621
Federico Gimenez <federico.gimenez@gmail.com>
2020-01-25 15:55:07 +0100

    implement matrixbot.newTransaction

--- src/matrixbot.js
+++ src/matrixbot.js
@@ -6,8 +6,36 @@ class Matrixbot {
     this.endpoint = cfg.endpoint
   }
 
-  newTransaction(name, address) {
+  newTransaction(data) {
+    const json = _transactionMsg(data)
 
+    return _send(json)
+  }
+
+  _transactionMsg(data) {
+    return {
+      "receiver": "webhook",
+      "status": "firing",
+      "alerts": [
+        {
+          "status": "firing",
+          "labels": {
+            "alertname": "TransactionSent",
+            "severity": "info"
+          },
+          "annotations": {
+            "description": `New transaction sent from account ${data.sender}, check https://polkascan.io/pre/${data.networkId}/account/${data.account}#transactions for details`
+          }
+        }
+      ],
+      "version": "4"
+    }
+  }
+
+  _send(json) {
+    return got.post(this.endpoint, {
+      json
+    })
   }
 }
 
