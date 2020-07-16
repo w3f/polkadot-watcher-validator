e8884728133b342e9dabcb895897cf3fc4512808
pampatzoglou <15629182+pampatzoglou@users.noreply.github.com>
2020-07-16 16:29:53 +0300

    Update patch.sh

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 set -e
 
-
-patch(){
-  local BRANCH=$1
-  local LATEST_UPSTREAM=$2
-  local GITHUB_BOT_TOKEN=$3
-
-  local build_dir=$(mktemp -d)
-  local target_repo="https://web3bot:${GITHUB_BOT_TOKEN}@github.com/w3f/polkadot-watcher"
-
-  cd $build_dir
-  git clone "${target_repo}" .
-  git checkout -b "${BRANCH}"
-
-  sed -i "/parity\/polkadot/c\      - image: parity\/polkadot:$LATEST_UPSTREAM " .circleci/config.yml
-
-
-}
+echo patching...
+sed -i "/parity\/polkadot/c\      - image: parity\/polkadot:$latest_upstream " .circleci/config.yml
